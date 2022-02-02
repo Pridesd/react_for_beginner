a2bf599d1b7059a1726a26f70f5b268ece8ec263
Pridesd <whwotjr98@naver.com>
2022-02-02 19:03:17 +0900

    prop-types & module.css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import Button from './Button';
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Button text = "Click" />
   </React.StrictMode>,
   document.getElementById('root')
 );
